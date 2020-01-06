36400cc41d90f9482f99b4350c6b64dbb3010c86
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-01-06 17:38:03 +0200

    adding dynamic reading fields

--- react-learning-app/src/App.js
+++ react-learning-app/src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Mexawy', age: 21},
+      { name: 'Khalod', age: 211}
+
+    ],
+    otherState: "others"
+  }
+  switchNameHandler = (newName) => {
+    this.setState({   
+      persons: [
+        { name: newName, age: 28},
+        { name: 'Mexawylawyy law', age: 21},
+        { name: 'Khalod bodabod ', age: 211}
+    ]
+  })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({   
+      persons: [
+        { name: "MAAAAAAXXXX", age: 28},
+        { name: event.target.value, age: 21},
+        { name: 'Khalod bodabod ', age: 211}
+    ]
+  })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a reat App</h1>
-        <Person name="khaled" age="21" />
-        <Person name="khaled2" age="221" />
-        <Person name="khale3" age="23333" />
+        <button onClick={this.switchNameHandler.bind(this,"KhaledButton")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this,"KhaledText")}
+          changed={this.nameChangedHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
 
       </div>
     );
--- react-learning-app/src/Person/Person.js
+++ react-learning-app/src/Person/Person.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 
 const person = (props) => {
-  return <p>I'm a {props.name} and I'm {props.age} years old!</p>
+  return (
+    <div>
+  <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
+  <p>{props.children}</p>
+  <input type="text" onChange={props.changed} />
+  </div>
+)
 };
 
 export default person;
