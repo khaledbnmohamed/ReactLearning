2a2c54934caf3fe773513b3851baceec14f49d90
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-01-15 02:01:27 +0200

    Test-Drive-TicTacToe

--- react-learning-app/src/Person/Person.js
+++ react-learning-app/src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 
 const person = (props) => {
   return (
-    <div>
-  <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
-  <p>{props.children}</p>
-  <input type="text" onChange={props.changed} />
+    <div className="Person">
+        <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
   </div>
 )
 };
